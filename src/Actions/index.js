91e115660284dcd3ad18c4cbb8d7f73b6b960218
Renata Monteiro <60657822+renataapmonteiro@users.noreply.github.com>
2020-10-26 20:15:38 -0300

    etapa 4 reducers e api implentadas inicialmente

---
+++ src/Actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import BASE_URL from '../Api/Api';
+
+export const FETCHED_DATA = 'FETCHED_DATA';
+
+export function fetchedDataCurrency(currencyPair){
+    
+    const Url = `${BASE_URL}${currencyPair}&start=1546300800&end=1546646400&period=14400`;
+    const request = axios.get(Url);
+
+    console.log(request);
+
+    return {
+        type: FETCHED_DATA,
+        payload: request
+    }
+}
