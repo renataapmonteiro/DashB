94448f7895699ac0a6a586b9a96fb7a6a28562ca
Renata Monteiro <60657822+renataapmonteiro@users.noreply.github.com>
2020-10-26 18:16:44 -0300

    Implementando a etapa um de construção de comps.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import './App.css';
+import './App.module.css';
+import Header from './components/Header';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>App</h1>
+      <Search />
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Search extends React.Component{
+    render(){
+        return (
+            <div >
+                <h1>Search</h1>   
+            </div>
+        )
+    }
+}
+
+
+export default Search;
+    
