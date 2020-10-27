2e6fbce8ca5d919b12978c85d167de523bc5be3f
Renata Monteiro <60657822+renataapmonteiro@users.noreply.github.com>
2020-10-26 23:51:18 -0300

    etapa 7 graphs list

--- src/components/CurrencyDataList.js
+++ src/components/CurrencyDataList.js
@@ -1,14 +1,59 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 
 class CurrencyDataList extends React.Component {
+
+    renderData(moneyData){
+        const highs = moneyData.map( dataCurrency => dataCurrency.high);
+        console.log(highs);
+        const lows = moneyData.map( dataCurrency => dataCurrency.low);
+        console.log(lows);
+        const opens = moneyData.map( dataCurrency => dataCurrency.open);
+        console.log(opens);
+        const closes = moneyData.map( dataCurrency => dataCurrency.close);
+        console.log(closes);
+        const volumes = moneyData.map( dataCurrency => dataCurrency.volume);
+        console.log(volumes);
+        const weightedAverages = moneyData.map( dataCurrency => dataCurrency.weightedAverage);
+        console.log(weightedAverages);
+
+        return(
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        )
+    }
+
     render (){
         return (
-            <div>
-               <h1>Currency</h1>
-            </div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Maior Preço</th>
+                        <th>Menor Preço</th>
+                        <th>Open - Preço Inicial</th>
+                        <th>Close - Preço final</th>
+                        <th>Volume</th>
+                        <th>Valor total Negociado</th>
+                        <th>Valor Medio da Moeda</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.dataCurrency.map(this.renderData)}
+                </tbody>
+            </table>
         )
     }
 }
 
-export default CurrencyDataList
+function mapStateToProps({ dataCurrency }){
+    return { dataCurrency }
+}
+
+export default connect(mapStateToProps)(CurrencyDataList);
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,12 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-
         <nav>
             <div className=" nav-wrapper boxBloo">
                 <Link to='/' className="item">Rank</Link>
                 <Link to='/cdl' className="item">Currency Data</Link>   
             </div>
         </nav>
-        
     )
 }
 export default Header;
